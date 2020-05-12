326053694a4f09031c12d409d4d00a6aaf536db3
Diego Moreno <iam@diegomoreno.co>
2020-05-12 00:26:00 -0500

    Firebase keys added to project

--- src/config/index.js
+++ src/config/index.js
@@ -1,1 +1,9 @@
-// Config to connect with database
+export const firebaseConfig = {
+  apiKey: "AIzaSyDqCnSsR2tyW8fNGi4-6wbcgmoOsUELb78",
+  authDomain: "backend-nodejs-ed79a.firebaseapp.com",
+  databaseURL: "https://backend-nodejs-ed79a.firebaseio.com",
+  projectId: "backend-nodejs-ed79a",
+  storageBucket: "backend-nodejs-ed79a.appspot.com",
+  messagingSenderId: "982889679689",
+  appId: "1:982889679689:web:909b60f5d110880fd1e898"
+};
--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import express from 'express';
 import * as bodyParser from 'body-parser';
 
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+import { firebaseConfig } from './config';
+
+firebase.initializeApp(firebaseConfig);
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); 
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
